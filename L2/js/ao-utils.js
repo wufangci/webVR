be6e97f059636d4ffb79fbba831bee4dc3718068
wufangci <cicichen0825@gmail.com>
2022-02-05 14:34:04 +0800

    add : 環境光源

--- L2/js/ao-utils.js
+++ L2/js/ao-utils.js
@@ -1,12 +1,14 @@
-
-AFRAME.registerComponent('ao', { 
-    init: function () {
-        let mesh = this.el.getObject3D('mesh');
-        if (mesh) {
-            let geometry = mesh.geometry;
-            if (geometry) {
-                geometry.addAttribute( 'uv2', new THREE.BufferAttribute( geometry.attributes.uv.array, 2 ) );
-            }
-        }
+AFRAME.registerComponent("ao", {
+  init: function () {
+    let mesh = this.el.getObject3D("mesh");
+    if (mesh) {
+      let geometry = mesh.geometry;
+      if (geometry) {
+        geometry.addAttribute(
+          "uv2",
+          new THREE.BufferAttribute(geometry.attributes.uv.array, 2)
+        );
+      }
     }
+  },
 });
